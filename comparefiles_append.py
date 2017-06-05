30d3bf613b8656454e3fcd728ddde3b96b6753fb
rmuppala113 <rakeshmuppala113@gmail.com>
2017-06-05 12:42:30 -0700

    Update comparefiles_append.py

--- comparefiles_append.py
+++ comparefiles_append.py
@@ -1,5 +1,7 @@
-f1 = open('pwdv1.0','r')
-f2 = open('pwdv2.0','r')
+#!/usr/bin/env python
+
+f1 = open('pswd_rh7','r')
+f2 = open('pswd_rh5','r')
 
 f1_list = []
 f2_list = []
@@ -16,20 +18,20 @@ for f2line in f2.readlines():
 print("\nFile1 has {} lines\nFile2 has {} lines\n".format(len(f1_list),len(f2_list)))
 
 for element in f2_list:
-    if element not in f1_list:
+    if not element.startswith("root:") and element not in f1_list:
         common.append(element)
 
 f1_list.extend(common)
-f1append = open('pwdv1.0','a')
+f1append = open('pswd_rh7','a')
 if len(common) > 0:
-    print("There are {} lines that present in file2 but not in file1\n".format(len(common)))
+    print("There are {} lines that present in file2 but not in file1\n\nBelow lines from file2 will be appended to File1\n".format(len(common)))
     for l in common:
          la = l.strip()
          if la:
              print(la)
              f1append.write(la + '\n')
 else:
-    print("Nohting found, both files has same data")
+    print("Nothing found, Both files has same data")
 
 f1.close()
 f2.close()
